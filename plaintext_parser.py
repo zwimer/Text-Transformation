51946015b1ee6ff6a2e573623db7a4ecb0e2670f
charlie <schmic6@rpi.edu>
2017-11-15 14:00:05 -0500

    plaintext is essentially done, maybe except for contractions

--- plaintext_parser.py
+++ plaintext_parser.py
@@ -3,20 +3,36 @@ import re
 
 # Input: String
 # Output: list of list of strings (lower cased individual words)
+# TODO: Need to fix contractions i.e. don't, won't, etc.
 def parse(content):
 
     print("this is original:\n", content)
 
-    translator = str.maketrans('', '', string.punctuation)
+    # Lower case all words
+    content = content.lower()
+
+    # Remove all symbols except # and > (for use with stop punctuation later)
+    punct = re.sub("[#>]", "", string.punctuation)
+    translator = str.maketrans("", "", punct)
     parsed = content.translate(translator)
-    parsed = parsed.lower()
 
     # First, need to split on escape characters
     # (except html tags; this is done by HTML parser)
-    word_list = re.split('\n\t|\n\n|\n\#', parsed)
+    word_list = re.split("\n\t|\n\n|\n\#|\n>", parsed)
 
-    # Second, need to split on whitespace to break up words
+    # Second, need to remove extraneous # and >
+    # that were not part of stop punctuation
+    new_word_list = []
+    for word in word_list:
+        translator = str.maketrans("", "", "#>")
+        new_word_list.append( word.translate(translator) )
 
-    print("this is word list:\n", word_list)
+    # Finally, split on whitespace to get individual words
+    list_of_lists = []
+    for word in new_word_list:
+        section = word.split()
+        list_of_lists.append( section )
 
-    return parsed
+    print("this is word list:\n", list_of_lists)
+
+    return list_of_lists

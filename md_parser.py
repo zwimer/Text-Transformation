8462ed54392059edb427c5477bc1491b66b3275b
charlie <schmic6@rpi.edu>
2017-11-20 15:21:35 -0500

    added link and image extraction to MD parser yay!

--- md_parser.py
+++ md_parser.py
@@ -9,12 +9,18 @@ class MarkdownParser:
         """
 
     # Input: Giant string of markdown content
-    # Outblockput: List of list of strings (lower cased individual words)
+    # Output: List of list of strings (lower cased individual words)
     def parse(self, content):
         # Remove all code blocks
         code_blocks = r'`(.*?(\s)*?)*?`'
-        new_content = re.sub(code_blocks, " ", content)
+        code_free_content = re.sub(code_blocks, " ", content)
 
+        # Remove links (this will include image blocks--the ! symbol is removed
+        # by plaintext later on)
+        link_blocks = r'\[(.*?)\][\[\(].*?[\]\)]'
+        regex = re.compile(link_blocks)
+        new_content = regex.sub("\\1", code_free_content)
+        
         # May need to go line by line to feed into plaintext_parser here...
         PTParser = plaintext_parser.PlaintextParser()
         return PTParser.parse(new_content)

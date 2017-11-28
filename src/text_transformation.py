a465b056a98e6d3bfca8286795c8ce39121b88db
Lee Cattarin <lee.cattarin@gmail.com>
2017-11-28 13:21:05 -0500

    Changes to main and slight edits to faulty test case

--- src/text_transformation.py
+++ src/text_transformation.py
@@ -1,48 +1,10 @@
 import sys
 import json
 
-# import Bs4Wrapper <- currently has syntax errors
-import plaintext_parser
-import md_parser
+import html_sanitizer
+import markdown_sanitizer
+import text_sanitizer
 import ngram_creator
-
-def get_ngrams(text):
-    """
-    param text: list of lists of strings used for ngram creation
-    returns: a dictionary of ngrams and their indices, divided by number
-        of terms
-    """
-    # Get stopwords from indexing
-    stopwords = ["my"]
-
-    # Compile ngram_creator's results
-    return {1 : ngram_creator.create(text, 1, stopwords, False), \
-            2 : ngram_creator.create(text, 2, stopwords, True), \
-            3 : ngram_creator.create(text, 3, stopwords, True)}
-
-def parse(text, file_type):
-    """
-    param text: full text to be parsed
-    """
-    # Process html files
-    if file_type == "html":
-        # html_parser = Bs4Wrapper.Bs4Wrapper(text)
-
-        # html_parser.clear_comments()
-        # text_lists = html_parser.extract_plain_text_lists():
-        pass
-
-    if file_type == "md":
-        # markdown parser - needs to be fixed slightly
-        # MDParser = md_parser.MarkdownParser()
-        # text_lists = MDParser.parse(text)
-        pass
-
-    # parse plain text
-    # PTParser = plaintext_parser.PlaintextParser()
-    # parsed_plaintext_content = PTParser.parse(text_lists)
-
-    return [["my", "amazing", "book"], ["hello", "world", "book"]]
 
 def arg():
     """
@@ -62,18 +24,112 @@ def arg():
 
     return filename, file_type
 
+def parse(text, file_type):
+    """
+    param text: full text to be parsed
+    returns a tupe with (title string, metadata dictionary, parsed text lists)
+    """
+
+    print("DEBUG: Parsing as {}".format(file_type))
+
+    # Create parser based on file type
+    if file_type == "html":
+        parser = html_sanitizer.HtmlSanitizer(text)
+    elif file_type == "md":
+        parser = markdown_sanitizer.MarkdownSanitizer(text)
+    elif file_type == "txt":
+        parser = text_sanitizer.TextSanitizer(text)
+
+    return (parser.get_title(), parser.get_metadata(), parser.sanitize())
+
+def get_title_indices(title, text_lists):
+    """
+    param title: the title string (can be None)
+    param text_lists: list of list of strings of sanitized text
+    returns: a list of indices where the title is found in the text
+    """
+
+    # no title -> return an empty list
+    if title == None:
+        return []
+
+    # Count the term index
+    index = 0
+    indices = []
+
+    title_terms = title.split()
+    title_len = len(title)
+
+    for section in text_lists:
+
+        # if the start of the title isn't in this section, skip it
+        if not title_terms[0] in section:
+            index += len(section)
+            continue
+
+        for term, i in enumerate(section):
+            if term == title_terms[0]:
+
+                # when the first term is found, check the title_terms list
+                # against a sublist from section of the same length as the title
+                if title_terms == section[i:i+title_len]:
+                    indices.append(index)
+
+            index += 1
+
+    print("DEBUG:\tFound title {} at indices: ".format(title))
+    print("\t",indices)
+
+    return indices
+
+def get_ngrams(text):
+    """
+    param text: list of lists of strings used for ngram creation
+    returns: a dictionary of ngrams and their indices, divided by number
+        of terms
+    """
+    # Get stopwords from indexing
+    # TODO add call to indexing
+    # TODO change for tests
+    stopwords_fname = "temp/english_stopwords.json"
+    stopwords = json.load( open(stopwords_fname, "r") )
+
+    print("DEBUG:\tLoaded list of {} stopwords".format(len(stopwords)))
+
+
+    # Compile ngram_creator's results
+    return {1 : ngram_creator.create(text, 1, stopwords, False), \
+            2 : ngram_creator.create(text, 2, stopwords, True), \
+            3 : ngram_creator.create(text, 3, stopwords, True)}
+
+# =============================================================================
+# MAIN
+# =============================================================================
 
 def main( arguments ):
     """
     param arguments: a tuple containing the name of the file to be used and
-        its type. Type is assumed to be 'txt', 'html', or 'md'.
+        its type. Allowable types are 'txt', 'html', or 'md'.
     """
+
+    # Check for validity
+    if len(arguments) != 2:
+        print("ERROR: arguments must be filename, file_type")
+        sys.exit()
+
+    # TODO this will come from a different source eventually
     filename = arguments[0]
     file_type = arguments[1]
 
+    types_allowed = ("txt", "md", "html")
+
+    if not file_type in types_allowed:
+        print("ERROR: Unusable file type")
+        sys.exit()
+
     # Open and read in file
     try:
-        f = open(filename, "r")
+        f = open(filename, "r", encoding="utf-8")
     except:
         print("ERROR: File failed to open")
         sys.exit()
@@ -84,27 +140,24 @@ def main( arguments ):
     # eventual full output dictionary
     output = {}
 
-    text_list = parse(text, file_type)
+    # parse text
+    (text_list, metadata, title) = parse(text, file_type)
+    # TODO remove dummy values
+    text_list = [["my", "amazing", "book"], ["hello", "world", "book"]]
+    title = "my amazing book"
 
-    # figure out division of responsibilities between parse etc for
-    # metadata...
-    """
-    if file_type == "html":
-        output["title"] = html_parser.get_title()
-        output["metadata"] = html_parser.get_metadata()
-        # del metadata if empty
-        pass
-    else:
-        # set title fields to empty
-        output["title"] = { "title": "", "indices": [] }
-    """
+    # metadata will either be populated or an empty dictionary
+    output["metadata"] = metadata
+
+    # title is either a string or none
+    output["title"] = { "title" : title }
+    output["title"]["indices"] = get_title_indices(title, text_list)
+
     # Get ngrams and add to output
     output["ngrams"] = get_ngrams(text_list)
 
-    # get title from ngram output
-
     # Write to file as JSON
-    # output file name should be programmatically generated
+    # TODO output file name should be programmatically generated
     try:
         fout = open("dummy.json", "w")
     except:
@@ -117,3 +170,4 @@ def main( arguments ):
 
 if __name__ == "__main__":
     main(arg())
+    # TODO connection from crawling?
